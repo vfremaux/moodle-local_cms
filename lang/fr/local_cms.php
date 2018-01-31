bc1da4015d961ec0b49e929b3b929a8010ae34e5
valery <valery.fremaux@gmail.com>
2018-01-31 14:41:25 +0100

    Add missing branches

--- lang/fr/local_cms.php
+++ lang/fr/local_cms.php
@@ -1,4 +1,18 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 $string['cms:manageblocks'] = 'Gérer les blocs';
 $string['cms:manageview'] = 'Gérer les vues';
@@ -51,6 +65,7 @@ $string['menudeleted'] = 'volume supprimé!';
 $string['menudeletesure'] = 'Vous êtes sur le point de <strong>supprimer</strong> le menu <strong>{$a}</strong>!<br />Voulez-vous continuer ?';
 $string['menus'] = 'Volumes CMS';
 $string['missingtitle'] = 'Le champ \'nom du lien\' est obligatoire';
+$string['modulenameplural'] = 'Pages statiques';
 $string['nameinuse'] = 'Il existe déjà une page nommée {$a}. Choisissez un autre nom';
 $string['nametooshort'] = 'Ce nom est vide ou est trop court';
 $string['newpage'] = 'Nouvelle page CMS';
